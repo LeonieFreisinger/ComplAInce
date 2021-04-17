b2a9afb11f7d8897977e2bfa2d72f9411cee3a9f
Leonie <ge79kej@mytum.de>
2021-04-17 18:59:12 +0200

    add link extraction

--- playground/leonie/extract_link.py
+++ playground/leonie/extract_link.py
@@ -2,40 +2,51 @@ import os
 import regex
 import mdutils
 import markdown
-from lxml import etree
+from lxml import etree, html
+import json
 
 
+## import files
 md_files = []
 CWD= os.getcwd()
 file = open(os.path.join(CWD, "test_test.md"))
 text = file.read()
-# print(text)
 file.close()
 
-# href_regex = r'href=[\'"]?([^\'" >]+)'
-# urls = regex.findall(href_regex, text)
 
-# print(urls)
+# body_markdown = "This is an [inline link](http://google.com). This is a [non inline link][1]\r\n\r\n  [1]: http://yahoo.com"
+body_markdown = text
+doc = html.fromstring(markdown.markdown(body_markdown))
+# print(markdown.markdown(body_markdown))
+link_data = []
+#link_text_data = []
+chapter = []
+for title in doc.xpath('//h2/text()'):
+    data = {
+        "title" : title, 
+    }
+    chapter.append(data)
+print(chapter[0])
 
-    #with open(path) as f:
-      #  data = f.read()
-# rex = """(?|(?<txt>(?<url>(?:ht|f)tps?://\S+(?<=\P{P})))|\(([^)]+)\)\[(\g<url>)\])"""
-# pattern = regex.compile(rex)
-matches = regex.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text, overlapped=True)
-#print(matches)
-# for link in matches:
-   #print ("\n ",link)
-
-
-# text = text.split(' ')
-# rex_2'\(.*?\)'
-
-body_markdown = "This is an [inline link](http://google.com). This is a [non inline link][1]\r\n\r\n  [1]: http://yahoo.com"
-#body_markdown = text
-# print(body_markdown)
-md = markdown.markdown(body_markdown)
-print(md)
-doc = etree.fromstring(md)
-#print(doc.xpath('//a')[0].text)
 for link in doc.xpath('//a'):
-     print(link.text, link.get('href'))
+    data = {
+        "link" : link.get('href'),
+        "link_text": link.text
+    }
+    link_data.append(data)
+
+link_all= {
+    "chapter": chapter[0],
+    "data": link_data
+}
+    #link_data.append(link.get('href'))
+    #link_text_data.append(link.text)
+# print(len(link_data), len(link_text_data))    
+#dict_links = dict(zip(link_data, link_text_data))
+# dict_links = dict(zip(link_data, chapter))
+with open('chapter_links.json', 'w') as fp:
+    json.dump(link_all, fp)
+
+with open('chapter.json', 'w') as fp:
+    json.dump(chapter, fp)
+print(chapter)
