074acd9ad06e7eea6cb208ff84e26be7196de24a
Leonie <ge79kej@mytum.de>
2021-04-17 23:46:25 +0200

    add file extract du utils

---
+++ backend/utils/extract_link.py
@@ -0,0 +1,65 @@
+import os
+import regex
+import mdutils
+import markdown
+from lxml import etree, html
+import json
+import difflib
+import mammoth
+#import pandoc
+
+def convert_doc_to_txt(filepath, docx_file):
+    with open(os.path.join(filepath, docx_file), "rb") as docx_file:
+        result = mammoth.convert_to_markdown(docx_file)
+    text = result.value
+    text= text.replace("[_", "[")
+    text= text.replace("_]", "]")    
+    return text
+
+def extract_link_to_json(text, docx_file):
+    doc = html.fromstring(markdown.markdown(text))
+    link_data = []
+    chapter = []
+
+    for link in doc.xpath('//a'):
+        data = {
+        "link" : link.get('href'),
+        "link_text": link.text
+        }
+        # print(data)
+        if data["link"] is None or data["link_text"] is None:
+            continue
+        sim_index = 0
+        for element in link_data:
+            sim= difflib.SequenceMatcher(None,element["link_text"],data["link_text"]).ratio()*100
+            if sim > 80:
+                sim_index=1
+        if sim_index == 0:
+            link_data.append(data)
+   
+    link_all= {
+    "chapter": docx_file,
+    "data": link_data
+    }
+
+    with open("chapter_links.json", "r+") as file:
+        data = json.load(file)
+        data['data'].append(link_all)
+        file.seek(0)
+        json.dump(data, file)    
+
+         
+
+filepath = ('../data/docx')
+if os.path.exists('chapter_links.json'):
+    os.remove('chapter_links.json')
+with open('chapter_links.json', 'w') as file:
+    json.dump({
+        "data": []
+    }, file)
+for docx_file in os.listdir(filepath):
+    #print(docx_file)
+    text = convert_doc_to_txt(filepath, docx_file)
+    extract_link_to_json(text, docx_file)
+    
+                
--- playground/leonie/extract_link.py
+++ playground/leonie/extract_link.py
@@ -4,6 +4,7 @@ import mdutils
 import markdown
 from lxml import etree, html
 import json
+import difflib
 
 
 ## import files
@@ -11,42 +12,43 @@ md_files = []
 CWD= os.getcwd()
 file = open(os.path.join(CWD, "test_test.md"))
 text = file.read()
+text= text.replace("[_", "[")
+text= text.replace("_]", "]")
 file.close()
 
 
 # body_markdown = "This is an [inline link](http://google.com). This is a [non inline link][1]\r\n\r\n  [1]: http://yahoo.com"
 body_markdown = text
 doc = html.fromstring(markdown.markdown(body_markdown))
-# print(markdown.markdown(body_markdown))
 link_data = []
-#link_text_data = []
 chapter = []
 for title in doc.xpath('//h2/text()'):
-    data = {
-        "title" : title, 
-    }
-    chapter.append(data)
-print(chapter[0])
+    chapter.append(title)
+
 
 for link in doc.xpath('//a'):
     data = {
         "link" : link.get('href'),
         "link_text": link.text
     }
-    link_data.append(data)
-
+    sim_index = 0
+    for element in link_data:
+        sim= difflib.SequenceMatcher(None,element["link_text"],data["link_text"]).ratio()*100
+        if sim > 80:
+            sim_index=1
+    if sim_index == 0:
+        link_data.append(data) 
+    #[f(x) for x in sequence if condition]
+    
 link_all= {
     "chapter": chapter[0],
     "data": link_data
 }
-    #link_data.append(link.get('href'))
-    #link_text_data.append(link.text)
-# print(len(link_data), len(link_text_data))    
-#dict_links = dict(zip(link_data, link_text_data))
-# dict_links = dict(zip(link_data, chapter))
+
 with open('chapter_links.json', 'w') as fp:
     json.dump(link_all, fp)
 
 with open('chapter.json', 'w') as fp:
     json.dump(chapter, fp)
-print(chapter)
+
+print(link_data)
